68fdc66a5dacc25a65f71043ce0dcd0844086d3e
alexjklimb <alexjklimb@gmail.com>
2020-08-14 09:05:18 -0800

    added the components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Parent from './Components/Parent/Parent';
 
 function App() {
   return (
---
+++ src/Components/ChildDisplay/ChildDisplay.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import 'ChildDisplay.css';
+function ChildDisplay(props){
+    return(<div>
+
+    </div>)
+}
+export default ChildDisplay;
