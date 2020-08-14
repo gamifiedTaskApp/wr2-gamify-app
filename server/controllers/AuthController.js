3f78d136aa57c761fdbc4abab48391749431d099
mattostraszewski <mrostraszewski@gmail.com>
2020-08-14 12:55:58 -0400

    login connects to db

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -12,7 +12,7 @@ module.exports = {
 
     const checkEmail = await db.auth.check_email_exists(email);
     if (checkEmail[0]) {
-      res.status(409).send('Email is already assigned to an account')
+      res.status(409).send('Email is already assigned to an account');
     };
 
     const salt = bcrypt.genSaltSync(10);
@@ -29,12 +29,34 @@ module.exports = {
       parental: user.is_parental,
       points: user.experience_points
     };
-    console.log(req.session.user)
+    // console.log(req.session.user)
     res.status(200).send(req.session.user);
   },
 
   login: async (req, res) => {
     const db = req.app.get('db');
+    const { username, password } = req.body;
 
+    const checkUsername = await db.auth.check_username_exists(username);
+    const user = checkUsername[0];
+    if (!user) {
+      res.status(404).send('Sorry can not seem to find that username. Try again!');
+    };
+
+    const passCheck = bcrypt.compareSync(password, user.password);
+    if (!passCheck) {
+      res.status(403).send('Password is incorrect');
+    };
+
+    req.session.user = {
+      id: user.user_id,
+      firstName: user.first_name,
+      lastName: user.last_name,
+      email: user.email,
+      parental: user.is_parental,
+      points: user.experience_points
+    };
+    // console.log(req.session.user, 'hitter')
+    res.status(200).send(req.session.user);
   }
-}
+};
