00419bc4c6f7309706db5c8ee01c9d1a58748568
GrantTabor <grantmtabor@gmail.com>
2020-08-17 17:27:42 -0500

    children can be made and log in

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -43,27 +43,43 @@ module.exports = {
     const { username, password } = req.body;
 
     const checkUsername = await db.auth.check_username_exists(username);
-    const user = checkUsername[0];
-    if (!user) {
-      res.status(404).send('Sorry can not seem to find that username. Try again!');
-    };
-    console.log(user)
-    const passCheck = bcrypt.compareSync(password, user.password);
-    if (!passCheck) {
-      res.status(403).send('Password is incorrect');
-    };
+    const checkChildUsername = await db.auth.check_child_username(username);
 
-    req.session.user = {
-      id: user.user_id,
-      username: user.username,
-      firstName: user.first_name,
-      lastName: user.last_name,
-      email: user.email,
-      parental: user.is_parental,
-      points: user.experience_points
-    };
-    // console.log(req.session.user, 'hitter')
-    res.status(200).send(req.session.user);
+    const user = checkUsername[0];
+    const childUser = checkChildUsername[0];
+    if (user) {
+      const passCheck = bcrypt.compareSync(password, user.password);
+      if (!passCheck) {
+        res.status(403).send('Password is incorrect');
+      };
+      req.session.user = {
+        id: user.user_id,
+        username: user.username,
+        firstName: user.first_name,
+        lastName: user.last_name,
+        email: user.email,
+        parental: user.is_parental,
+        points: user.experience_points
+      };
+      // console.log(req.session.user, 'hitter')
+      res.status(200).send(req.session.user);
+    }
+    else if (childUser){
+      const passCheck = bcrypt.compareSync(password, childUser.password);
+      if (!passCheck) {
+        res.status(403).send('Password is incorrect');
+      };
+      req.session.user = {
+        id: childUser.child_id,
+        username: childUser.child_username,
+        parent: childUser.u_id,
+        points: childUser.points
+      };
+      res.status(200).send(req.session.user);
+    }
+    else{
+      res.status(404).send('Sorry can not seem to find that username. Try again!');
+    }    
   },
   registerChild: async (req, res) => {
     const db = req.app.get("db");
--- src/ponents/Parent/Parent.js
+++ src/ponents/Parent/Parent.js
@@ -1,19 +1,35 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
+import Axios from "axios"
 import './parent.css';
-function Parent(props) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [name, setName] = useState('');
-  const [view, setView] = useState(false);
-  function Switch() {
-    if (view === false) {
-      setView(true)
-      console.log(view)
+import {connect} from "react-redux";
+function Parent(props){
+    const parentId = props.userReducer.user.data ? props.userReducer.user.data.id : "";
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [name, setName]=useState('');
+    const [view, setView] = useState(false);
+function Switch(){
+    if(view===false){
+        setView(true)
+        console.log(view)
     }
     else {
       setView(false)
     }
-  }
+}
+function createChild(){
+    console.log("creating")
+    Axios.post('/auth/register/child', {username, password, parentId})
+    .then(res => {
+        alert("child made")
+        Switch();
+        setUsername("");
+        setPassword("");
+        setName("");
+    })
+    .catch(err => alert(err));
+}
+
 
 
 
@@ -28,19 +44,19 @@ function Parent(props) {
             <button onClick={Switch}>+</button>
           </div>
         </div>
-      </div> :
-      <div>
-        <header>nav</header>
-        <div className="cont">
-          <div className="child_container">
-            <span className="input">Name<br></br><input value={name} onChange={e => setName(e.target.value)}></input></span>
-            <span className="input">Username<br></br><input value={username} onChange={e => setUsername(e.target.value)}></input></span>
-            <span className="input">Password<br></br><input type="password" value={password} onChange={e => setPassword(e.target.value)}></input></span>
-            <button className='button' onClick={Switch}>Submit</button>
-          </div>
+    </div> :
+    <div>
+    <header>nav</header>
+    <div className="cont">
+        <div className="child_container">
+        <span className="input">Name<br></br><input value={name} onChange={e => setName(e.target.value) }></input></span>
+        <span className="input">Username<br></br><input value={username} onChange={e => setUsername(e.target.value) }></input></span>
+        <span className="input">Password<br></br><input type="password" value={password} onChange={e => setPassword(e.target.value) }></input></span>
+          <button className='button' onClick={createChild}>Submit</button>
         </div>
       </div>
-  )
+      </div>
+    )
 }
 const mapStateToProps = reduxState => reduxState;
 
