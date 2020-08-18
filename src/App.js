40529e35f1952c7693c3036cfb943ee473271924
alexjklimb <alexjklimb@gmail.com>
2020-08-18 09:14:59 -0800

    added styling to navbar and changed routes to include Parent component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,11 @@ import React from 'react';
 import Nav from './ponents/Nav/Nav'
 import './App.css';
 import routes from './routes';
-import Parent from "./ponents/Parent/Parent"
 function App() {
   return (
     <div className="App">
-
-      <header className="App-header">
-        <p>
-          Grant branch
-        </p>
-
-      </header>
       <Nav />
       {routes}
-      <Parent />
     </div>
   );
 }
