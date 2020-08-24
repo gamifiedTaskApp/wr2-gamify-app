8f6579cf9c4e151059e9d0a85248be6076001905
alexjklimb <alexjklimb@gmail.com>
2020-08-24 12:16:00 -0600

    added navbar icon with toggle

--- src/ponents/Nav/Nav.js
+++ src/ponents/Nav/Nav.js
@@ -1,26 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import { withRouter, Link } from "react-router-dom";
 import "./Nav.css";
 
 const Nav = (props) => {
+  const [show, setShow] = useState(true);
+  function toggle(){
+    show?setShow(false):setShow(true)
+  }
   return (
     <div>
       {props.location.pathname !== "/login" &&
       props.location.pathname !== "/register" ? (
         <nav>
           <div className="nav">
-            <Link className="link" to="/tasks">
+            <h1>App Name</h1>
+            <button onClick={toggle} className="burger"><div></div><div></div><div></div></button>
+            <div className={show?"navbar":"show"}>
+            <Link onClick={toggle} className="link" to="/tasks">
               Tasks
             </Link>
-            <Link className="link" to="/store">
+            <Link onClick={toggle} className="link" to="/store">
               Store
             </Link>
-            <Link className="link" to="/rewards">
+            <Link onClick={toggle} className="link" to="/rewards">
               Rewards
             </Link>
-            <Link className="link" to="/profile">
+            <Link onClick={toggle} className="link" to="/profile">
               Profile
             </Link>
+            </div>
           </div>
         </nav>
       ) : null}
